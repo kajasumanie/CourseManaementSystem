7a96a02818e5f1181b68a9702ec6e76bf0f23d62
Kajasumanie Kanapathipillai <kkn@sl.tiqri.com>
2017-12-04 16:38:40 +0530

    implemented cource mangement system

---
+++ WebApplication3/WebApplication3/Data/DbInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+            // Look for any students.          
+            if (context.Students.Any())
+            {
+                return;   // DB has been seeded   
+            }
+
+            var students = new Student[]
+            {
+                new Student { FirstMidName = "Carson", LastName = "Alexander", PhoneNo="077059555",CourseName="llll",EnrollmentDate = DateTime.Parse("2005-0901") },
+                new Student { FirstMidName = "Meredith", LastName = "Alonso", PhoneNo="077059555",CourseName="llll", EnrollmentDate = DateTime.Parse("2002-0901") },
+                new Student { FirstMidName = "Arturo", LastName = "Anand", PhoneNo="077059555",CourseName="llll", EnrollmentDate = DateTime.Parse("2003-0901") },
+                new Student { FirstMidName = "Gytis", LastName = "Barzdukas", PhoneNo="077059555",CourseName="llll", EnrollmentDate = DateTime.Parse("2002-0901") },
+                new Student { FirstMidName = "Yan", LastName = "Li",  PhoneNo="077059555",CourseName="llll",EnrollmentDate = DateTime.Parse("2002-09-01") },
+                new Student { FirstMidName = "Peggy", LastName = "Justice", PhoneNo="077059555",CourseName="llll", EnrollmentDate = DateTime.Parse("2001-0901") },
+                new Student { FirstMidName = "Laura", LastName = "Norman", PhoneNo="077059555",CourseName="llll", EnrollmentDate = DateTime.Parse("2003-0901") },
+                new Student { FirstMidName = "Nino", LastName = "Olivetto",  PhoneNo="077059555",CourseName="llll",EnrollmentDate = DateTime.Parse("2005-0901") } };
+            foreach (Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
+
---
+++ WebApplication3/WebApplication3/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public string PhoneNo { get; set; }
+        public string CourseName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
